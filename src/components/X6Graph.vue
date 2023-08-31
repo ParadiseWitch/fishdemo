3e62a84f8fb697541a0d33a4a591af4f9639c6cd
Maid <zhp92@outlook.com>
2023-08-31 10:46:02 +0800

    update

--- src/components/X6Graph.vue
+++ src/components/X6Graph.vue
@@ -1,30 +1,111 @@
 <template>
-  <div style="width: 100%; height: 100%; position: relative">
-    <div id="x6-container" style="width: 100%; height: 100%"></div>
-    <div
-      id="minimap"
-      style="
-        position: absolute;
-        bottom: 25px;
-        right: 25px;
-        border-radius: 10px;
-        border: 2px rgb(197, 194, 194) solid;
-        box-shadow: 5px 5px 20px rgb(199, 199, 199);
-      "
-    ></div>
-    <!-- <button @click="btnOnClick">button</button> -->
+  <div class="graph-comp" style="width: 100%; height: 100%; position: relative">
+    <div id="stencil-container"></div>
+    <div id="x6-container" style=""></div>
+    <!-- <div id="dnd-container" class="dnd-wrap">
+      <div data-type="rect" class="dnd-rect" @mousedown="startDrag">
+        Rect
+      </div>
+      <div data-type="circle" class="dnd-circle" @mousedown="startDrag">
+        Circle
+      </div>
+    </div>
+    <div class="app-content" ref={this.refContainer} /> -->
   </div>
+  <div id="minimap"></div>
+  <button @click="e => btnOnClick('hand')">hand</button>
+  <button @click="e => btnOnClick('sel')">toggleHandTool</button>
 </template>
+
+<style scoped>
+.graph-comp {
+  display: flex;
+  padding: 0;
+  font-family: sans-serif;
+}
+
+#stencil-container {
+  position: relative;
+  width: 200px;
+  border: 1px solid #f0f0f0;
+}
+
+#x6-container {
+  width: 100%;
+  height: 100%;
+}
+
+#minimap {
+  position: absolute;
+  bottom: 25px;
+  right: 25px;
+  border-radius: 10px;
+  border: 2px rgb(197, 194, 194) solid;
+  box-shadow: 5px 5px 20px rgb(199, 199, 199);
+}
+
+#dnd-container {
+  height: 80vh;
+  min-width: 200px;
+  position: absolute;
+  left: 10px;
+  top: 20px;
+  /* z-index: 100; */
+  background-color: white;
+  border-radius: 10px;
+  border: 2px rgb(197, 194, 194) solid;
+  box-shadow: 5px 5px 20px rgb(199, 199, 199);
+}
+
+.dnd-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px;
+  padding: 16px;
+  border: 1px solid #f0f0f0;
+  user-select: none;
+}
+
+.dnd-rect {
+  width: 100px;
+  height: 40px;
+  margin: 16px;
+  line-height: 40px;
+  text-align: center;
+  border: 1px solid #8f8f8f;
+  border-radius: 6px;
+  cursor: move;
+}
+
+.dnd-circle {
+  width: 60px;
+  height: 60px;
+  margin: 16px;
+  line-height: 60px;
+  text-align: center;
+  border: 1px solid #8f8f8f;
+  border-radius: 100%;
+  cursor: move;
+}
+</style>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import useX6Graph from '@/util/x6-graph'
-import type { Graph } from '@antv/x6'
-const { useGraph, showGraph, toggleHandTool, useMiniMap } = useX6Graph()
+import type { Graph, Model } from '@antv/x6'
+import type { Dnd } from '@antv/x6-plugin-dnd';
+import { Stencil } from '@antv/x6-plugin-stencil'
+
+const { useGraph, showGraph, toggleHandTool, useMiniMap, useDnd, useStencil } = useX6Graph()
 
 const graphRef = ref<Graph>()
+const dndRef = ref<Dnd>()
+const stencilRef = ref<Stencil>()
 
-const data = {
+let isHand = true;
+
+const data: Model.FromJSONData = {
   nodes: [
     {
       id: 'node1',
@@ -69,7 +150,8 @@ const data = {
       shape: 'edge',
       source: 'node1',
       target: 'node2',
-      label: 'x6',
+      label: '×',
+      router: 'orth',
       attrs: {
         // line 是选择器名称，选中的边的 path 元素
         line: {
@@ -81,25 +163,69 @@ const data = {
   ]
 }
 
-const btnOnClick = () => {
+const startDrag = (e: MouseEvent) => {
   const graph = graphRef.value
   if (graph == undefined) {
     throw new Error('graph is undefined')
   }
-  toggleHandTool(false)
+  const dnd = dndRef.value
+  if (dnd == undefined) {
+    throw new Error('dnd is undefined')
+  }
+
+  const target = e.currentTarget as HTMLElement
+  if (target == null) return
+  const type = target.getAttribute('data-type')
+  const node =
+    type === 'rect'
+      ? graph.createNode({
+        width: 100,
+        height: 40,
+        label: 'Rect',
+        attrs: {
+          body: {
+            stroke: '#8f8f8f',
+            strokeWidth: 1,
+            fill: '#fff',
+            rx: 6,
+            ry: 6,
+          },
+        },
+      })
+      : graph.createNode({
+        width: 60,
+        height: 60,
+        shape: 'circle',
+        label: 'Circle',
+        attrs: {
+          body: {
+            stroke: '#8f8f8f',
+            strokeWidth: 1,
+            fill: '#fff',
+          },
+        },
+      })
+
+  dnd.start(node, e)
+}
+
+const btnOnClick = (type: string) => {
+  const graph = graphRef.value
+  if (graph == undefined) {
+    throw new Error('graph is undefined')
+  }
+  isHand = !isHand
+  toggleHandTool(isHand)
 }
 
 onMounted(() => {
   const container = document.getElementById('x6-container') as HTMLDivElement
+  const stencilContainer = document.getElementById("stencil-container") as HTMLElement
   const graph = useGraph(container)
   graphRef.value = graph
   useMiniMap(document.getElementById('minimap') as HTMLDivElement)
+  dndRef.value = useDnd(document.getElementById('dnd-container') as HTMLDivElement)
+  stencilRef.value = useStencil(stencilContainer)
   showGraph(data)
 })
 </script>
-
-<style>
-.x6-widget-minimap {
-  border-radius: 10px;
-}
-</style>
--- src/util/x6-graph/index.ts
+++ src/util/x6-graph/index.ts
@@ -1,4 +1,4 @@
-import { Graph } from '@antv/x6'
+import { Graph, Model } from '@antv/x6'
 import { Transform } from '@antv/x6-plugin-transform'
 import { Snapline } from '@antv/x6-plugin-snapline'
 import { Clipboard } from '@antv/x6-plugin-clipboard'
@@ -7,6 +7,9 @@ import { History } from '@antv/x6-plugin-history'
 import { Selection } from '@antv/x6-plugin-selection'
 import { Scroller } from '@antv/x6-plugin-scroller'
 import { MiniMap } from '@antv/x6-plugin-minimap'
+import { Dnd } from '@antv/x6-plugin-dnd'
+import { Stencil } from '@antv/x6-plugin-stencil'
+
 
 
 
@@ -58,25 +61,24 @@ const useX6Graph = () => {
       })
     )
 
-
-    // graph.use(
-    //   new Transform({
-    //     resizing: {
-    //       enabled: true,
-    //       minWidth: 1,
-    //       maxWidth: 200,
-    //       minHeight: 1,
-    //       maxHeight: 150,
-    //       orthogonal: false,
-    //       restrict: false,
-    //       preserveAspectRatio: false
-    //     },
-    //     rotating: {
-    //       enabled: true,
-    //       grid: 90
-    //     }
-    //   })
-    // )
+    graph.use(
+      new Transform({
+        resizing: {
+          enabled: true,
+          minWidth: 1,
+          maxWidth: 200,
+          minHeight: 1,
+          maxHeight: 150,
+          orthogonal: false,
+          restrict: false,
+          preserveAspectRatio: false
+        },
+        rotating: {
+          enabled: true,
+          grid: 90
+        }
+      })
+    )
     graph.use(
       new Snapline({
         enabled: true
@@ -165,8 +167,8 @@ const useX6Graph = () => {
   }
 
 
-  const showGraph = (data: any) => {
-    graph.fromJSON(data)
+  const showGraph = (data: Model.FromJSONData, options?: Model.FromJSONOptions | undefined) => {
+    graph.fromJSON(data, options)
     // graph.centerContent()
   }
 
@@ -177,6 +179,7 @@ const useX6Graph = () => {
       graph.disableRubberband()
     } else {
       // graph.panning.disablePanning()
+      graph.disablePanning()
       graph.enableMultipleSelection()
       graph.enableRubberband()
     }
@@ -194,11 +197,103 @@ const useX6Graph = () => {
     )
   }
 
+  const useDnd = (dndContainer: HTMLElement) => {
+    return new Dnd({
+      target: graph,
+      scaled: false,
+      dndContainer: dndContainer,
+    })
+  }
+
+  const useStencil = (stencilContainer: HTMLElement) => {
+    const stencil = new Stencil({
+      title: 'Stencil',
+      target: graph,
+      search(cell, keyword) {
+        return cell.shape.indexOf(keyword) !== -1
+      },
+      placeholder: 'Search by shape name',
+      notFoundText: 'Not Found',
+      collapsable: true,
+      stencilGraphHeight: 0,
+      groups: [
+        {
+          name: 'group1',
+          title: 'Group(Collapsable)',
+        },
+        {
+          name: 'group2',
+          title: 'Group',
+          collapsable: false,
+        },
+      ],
+    })
+    stencilContainer.appendChild(stencil.container)
+
+
+    const commonAttrs = {
+      body: {
+        fill: '#fff',
+        stroke: '#8f8f8f',
+        strokeWidth: 1,
+      },
+    }
+
+    const n1 = graph.createNode({
+      shape: 'rect',
+      x: 40,
+      y: 40,
+      width: 80,
+      height: 40,
+      label: 'rect',
+      attrs: commonAttrs,
+    })
+
+    const n2 = graph.createNode({
+      shape: 'circle',
+      x: 180,
+      y: 40,
+      width: 40,
+      height: 40,
+      label: 'circle',
+      attrs: commonAttrs,
+    })
+
+    const n3 = graph.createNode({
+      shape: 'ellipse',
+      x: 280,
+      y: 40,
+      width: 80,
+      height: 40,
+      label: 'ellipse',
+      attrs: commonAttrs,
+    })
+
+    const n4 = graph.createNode({
+      shape: 'path',
+      x: 420,
+      y: 40,
+      width: 40,
+      height: 40,
+      // https://www.svgrepo.com/svg/13653/like
+      path: 'M24.85,10.126c2.018-4.783,6.628-8.125,11.99-8.125c7.223,0,12.425,6.179,13.079,13.543c0,0,0.353,1.828-0.424,5.119c-1.058,4.482-3.545,8.464-6.898,11.503L24.85,48L7.402,32.165c-3.353-3.038-5.84-7.021-6.898-11.503c-0.777-3.291-0.424-5.119-0.424-5.119C0.734,8.179,5.936,2,13.159,2C18.522,2,22.832,5.343,24.85,10.126z',
+      attrs: commonAttrs,
+      label: 'path',
+    })
+
+    stencil.load([n1, n2], 'group1')
+    stencil.load([n3, n4], 'group2')
+
+    return stencil
+  }
+
   return {
     useGraph,
     showGraph,
     toggleHandTool,
     useMiniMap,
+    useDnd,
+    useStencil,
   }
 }
 
